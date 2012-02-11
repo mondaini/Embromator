c4c1fc9f014199b66068053dd98f77a17f562369
Filipe Mondaini <filipe@mondaini.me>
2012-02-10 23:37:59 -0200

    uso de mongodb, agora

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'heroku_app2797481',                      # Or path to database file if using sqlite3.
+        'USER': 'teste',                      # Not used with sqlite3.
+        'PASSWORD': 'testee',                  # Not used with sqlite3.
+        'HOST': 'ds029807.mongolab.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '29807',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
