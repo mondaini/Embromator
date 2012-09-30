f2ebf2713ef090d6ff6c79c43b96474b71438509
Filipe Mondaini <filipe@mondaini.me>
2012-09-30 12:07:14 -0300

    atualizando paths dos apps do django

--- embromator/settings.py
+++ embromator/settings.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'embromator.generator',
+    'generator',
     'south',
     'gunicorn',
 ]
